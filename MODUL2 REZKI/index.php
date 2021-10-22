4b59ca32c2d65be01dadf7a7ec0d1afbd3be1184
nando <muhammad.rezki.ananda@gmail.com>
2021-10-22 16:28:37 +0700

    Configure app routing

--- MODUL2 REZKI/index.php
+++ MODUL2 REZKI/index.php
@@ -1,13 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    // APP ROUTE
+    $project_location = "/WAD-REZKI-1202190044/MODUL2%20REZKI";
+    $me = $project_location;
 
-<head>
-  <?php include "component/helper/head.php"; ?>
-</head>
+    $request = $_SERVER['REQUEST_URI'];
 
-<body>
-
-<script src="assets/js/style.js"></script>
-</body>
-
-</html>
+    switch ($request) {
+        case $me.'/' :
+            require "pages/home.php";
+            break;
+        case $me.'/booking' :
+            require "pages/booking.php";
+            break;
+        case $me.'/mybooking' :
+            require "pages/my_booking.php";
+            break;
+        default:
+            http_response_code(404);
+            echo "404";
+            break;
+    }
+?>
---
+++ MODUL2 REZKI/pages/booking.php
@@ -0,0 +1,3 @@
+<?php
+echo "booking.php";
+?>
