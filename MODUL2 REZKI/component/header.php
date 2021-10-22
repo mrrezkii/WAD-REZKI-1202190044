60897f2f0b1849a5ab8faae56ffff48f88d17c98
nando <muhammad.rezki.ananda@gmail.com>
2021-10-22 21:51:45 +0700

    Add home page

--- MODUL2 REZKI/component/header.php
+++ MODUL2 REZKI/component/header.php
@@ -1,9 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <?php include "component/helper/head.php"; ?>
-</head>
-<body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
@@ -22,9 +16,3 @@
         </div>
     </div>
 </nav>
-
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</html>
--- MODUL2 REZKI/pages/home.php
+++ MODUL2 REZKI/pages/home.php
@@ -5,8 +5,89 @@
 </head>
 <body>
 <?php include "component/header.php"; ?>
-<main>
+<main class="mt-5">
+    <h5 class="text-center" style="padding-top: 10px;">WELCOME TO ESD VENUE RESERVATION</h5>
+    <section id="hotel">
+        <div class="container">
+            <h6 class="text-center text-light  bg-dark" style="padding: 5px;">Find your best deal for your event,
+                here!</h6>
+            <div class="row g-0">
+                <div class="col-lg-4 col-md-4 col-sm-6 d-flex justify-content-center">
+                    <div class="card" style="width: 22rem;">
+                        <img class="card-img-top"
+                             src="https://london.bridestory.com/images/c_fill,dpr_1.0,f_auto,fl_progressive,pg_1,q_80,w_680/v1/assets/IMG_0836_l7p7uz/ice-indonesia-convention-exhibition_wedding-at-ice-bsd1487580049_10.jpg"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Nusantara Hall</h5>
+                            <div class="card-text">
+                                <p class="text-secondary">$2000 / Hour</p>
+                                <p class="text-secondary">5000 / Capacity</p>
+                            </div>
+                        </div>
+                        <ul class="list-group list-group-flush text-center">
+                            <li class="list-group-item text-success">Free Parking</li>
+                            <li class="list-group-item text-success">Full Ac</li>
+                            <li class="list-group-item text-success">Cleaning Service</li>
+                            <li class="list-group-item text-success">Covid-19 Health Protocol</li>
+                            <li class="list-group-item bg-light">
+                                <a href="#" class="btn btn-primary">Book Now</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 d-flex justify-content-center">
+                    <div class="card" style="width: 22rem;">
+                        <img class="card-img-top"
+                             src="https://media-cdn.tripadvisor.com/media/photo-s/1a/25/06/c5/garuda-hall.jpg"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Garuda Hall</h5>
+                            <div class="card-text">
+                                <p class="text-secondary">$1000 / Hour</p>
+                                <p class="text-secondary">2000 / Capacity</p>
+                            </div>
+                        </div>
+                        <ul class="list-group list-group-flush text-center">
+                            <li class="list-group-item text-success">Free Parking</li>
+                            <li class="list-group-item text-success">Full Ac</li>
+                            <li class="list-group-item text-danger">No Cleaning Service</li>
+                            <li class="list-group-item text-success">Covid-19 Health Protocol</li>
+                            <li class="list-group-item bg-light">
+                                <a href="#" class="btn btn-primary">Book Now</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 d-flex justify-content-center">
+                    <div class="card" style="width: 22rem;">
+                        <img class="card-img-top"
+                             src="https://jabarprov.go.id/assets/images/berita/gambar_25191.jpg"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Gedung Serba Guna</h5>
+                            <div class="card-text">
+                                <p class="text-secondary">$500 / Hour</p>
+                                <p class="text-secondary">500 / Capacity</p>
+                            </div>
+                        </div>
+                        <ul class="list-group list-group-flush text-center">
+                            <li class="list-group-item text-danger">No Free Parking</li>
+                            <li class="list-group-item text-danger">No Full Ac</li>
+                            <li class="list-group-item text-danger">No Cleaning Service</li>
+                            <li class="list-group-item text-success">Covid-19 Health Protocol</li>
+                            <li class="list-group-item bg-light">
+                                <a href="#" class="btn btn-primary">Book Now</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+    </section>
 </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
