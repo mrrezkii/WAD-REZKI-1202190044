2ec122f8e3133eb876760bba120874b161221a7d
nando <muhammad.rezki.ananda@gmail.com>
2021-11-21 16:10:44 +0700

    Delete data

--- MODUL3 REZKI/Rezki_DetailBuku.php
+++ MODUL3 REZKI/Rezki_DetailBuku.php
@@ -65,14 +65,14 @@ include "transactions/Rezki_ReadBookById.php";
                             </div>
                             <div class="modal-body">
                                 <div class="form-group mb-4">
+                                    <input type="text" name="id"
+                                           value="<?= $data['id_buku'] ?>" hidden
+                                           required>
                                     <label class="fw-bold" for="title">Judul Buku</label>
                                     <input type="text" class="form-control mt-2" id="title" name="title"
                                            aria-describedby="titleHelp"
                                            placeholder="Contoh: Pemrograman Web Bersama EAD"
                                            value="<?= $data['judul_buku'] ?>"
-                                           required>
-                                    <input type="text" name="id"
-                                           value="<?= $data['id_buku'] ?>" hidden
                                            required>
                                 </div>
                                 <div class="form-group mb-4">
@@ -168,19 +168,28 @@ include "transactions/Rezki_ReadBookById.php";
             <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel"
                  aria-hidden="true">
                 <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="deleteModalLabel">Hapus</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <form action="transactions/Rezki_DeleteBook.php" method="POST">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="deleteModalLabel">Hapus</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <input type="text" name="id"
+                                       value="<?= $data['id_buku'] ?>" hidden
+                                       required>
+                                <input type="text" name="image"
+                                       value="<?= $data['gambar'] ?>" hidden
+                                       required>
+                                <p>Apakah Anda yakin ingin menghapus buku <?= $data['judul_buku'] ?> ?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                                <button type="submit" class="btn btn-danger">Hapus</button>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            <p>Apakah Anda yakin ingin menghapus buku (ini) ?</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                            <button type="button" class="btn btn-danger">Hapus</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </section>
--- MODUL3 REZKI/transactions/Rezki_DeleteBook.php
+++ MODUL3 REZKI/transactions/Rezki_DeleteBook.php
@@ -1,15 +1,20 @@
 <?php
 $conn = null;
-include "helper/error.php";
-include "helper/connection.php";
+include "../helper/error.php";
+include "../helper/connection.php";
 
 $id = $_POST['id'];
+$image = $_POST['image'];
+$dir = "../files/$image";
 
 try {
     $query = mysqli_query($conn,
-        "DELETE FROM Buku_Table WHERE id_buku = $id");
+        "DELETE FROM Buku_Table WHERE id_buku = '$id'");
     if ($query) {
-        header("Location: Rezki_Detail.php/?deleted=true");
+        if (file_exists($dir)) {
+            unlink($dir);
+        }
+        header("Location: ../Rezki_Home.php?deleted=true");
     } else {
         echo "<script>alert('Gagal delete');</script>";
     }
