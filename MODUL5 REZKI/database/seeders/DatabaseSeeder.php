9b87d5c1c9f76cb48dc3d73c294a1a9ce7de064a
nando <muhammad.rezki.ananda@gmail.com>
2021-12-09 23:34:40 +0700

    Configure db seeder

--- MODUL5 REZKI/database/seeders/DatabaseSeeder.php
+++ MODUL5 REZKI/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Patients;
+use App\Models\Vaccines;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Vaccines::factory(9)->create();
+        Patients::factory(10)->create();
     }
 }
