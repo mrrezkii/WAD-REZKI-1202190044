61b86ec56e4fd41293db9d66c0ca67cde86f4591
nando <muhammad.rezki.ananda@gmail.com>
2021-12-10 21:37:30 +0700

    Change name table rollback

--- MODUL5 REZKI/database/migrations/2021_12_10_095720_create_vaccine_table.php
+++ MODUL5 REZKI/database/migrations/2021_12_10_095720_create_vaccine_table.php
@@ -29,6 +29,6 @@ class CreateVaccineTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('vaccine');
+        Schema::dropIfExists('vaccines');
     }
 }
